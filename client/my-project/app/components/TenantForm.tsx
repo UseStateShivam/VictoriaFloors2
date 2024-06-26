1bd3acd76ee7b4368f84a78698d91a353619780a
Shivam Verma <mcrafter147@gmail.com>
2024-06-26 20:12:46 +0530

    Update TenantForm.tsx

--- client/my-project/app/components/TenantForm.tsx
+++ client/my-project/app/components/TenantForm.tsx
@@ -26,7 +26,7 @@ function TenantForm() {
         const RentAgreementSubmitted =  formData.get(TenantFormLabels[14])
 
         try {
-            const res = await axios.post('http://localhost:8000/api/auth/register', {
+            const res = await axios.post('https://victoria-project-api.vercel.app/auth/register', {
                 NameOfOwner,
                 NameOfTenant,
                 FlatNo,
