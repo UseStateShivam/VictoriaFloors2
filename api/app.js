7bffcdd77d31fddb006359b7282b22fe27fbb0b4
OptionsBuffetDev <mcrafter147@gmail.com>
2024-08-24 22:25:05 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // Update CORS options to handle multiple origins
 const corsOptions = {
-  origin: [process.env.CLIENT_URL, 'https://usestateshivam.github.io', 'https://usestateshivam.github.io/victoriaDeploy'],
+  origin: [process.env.CLIENT_URL, 'https://usestateshivam.github.io', 'https://usestateshivam.github.io/victoriaDeploy', 'http://localhost:3000'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true, // if you need to include cookies in CORS requests
   allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
